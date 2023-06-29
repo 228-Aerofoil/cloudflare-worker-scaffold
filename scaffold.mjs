a284e86a2009a0b09944f07900e55ca0324c4564
Michael Kassabov <35264484+Mkassabov@users.noreply.github.com>
2023-06-28 20:08:43 -0400

    update for multi-scaffold

--- scaffold.mjs
+++ scaffold.mjs
@@ -1,28 +1,38 @@
+import { getProjectRootPath } from "@aerofoil/aerofoil-core/util/getProjectRootPath";
 import fs from "fs-extra";
 import path from "path";
 import { fileURLToPath } from "url";
 
-const directory = path.resolve(
+const sourceDirectory = path.resolve(
   path.dirname(fileURLToPath(import.meta.url)),
   "source"
 );
 
-export async function scaffold({ deploymentName, deploymentPath }) {
-  await fs.copy(directory, deploymentPath);
-  const packageJSON = await fs.readJSON(
-    path.resolve(deploymentPath, "package.json")
+export async function scaffold({
+  generateDeploymentInfo,
+  generateDatabaseInfo,
+  deploymentTypes,
+  addTodos,
+}) {
+  const rootPath = await getProjectRootPath();
+  const deployInfo = await generateDeploymentInfo(null, {
+    deployment: { type: "cloudflare-worker" },
+  });
+  const deploymentRootPath = path.resolve(
+    rootPath,
+    "deployments",
+    deployInfo.name
   );
-  packageJSON.name = deploymentName;
+  await fs.copy(sourceDirectory, deploymentRootPath);
+  const packageJSON = await fs.readJSON(
+    path.resolve(deploymentRootPath, "package.json")
+  );
+  packageJSON.name = deployInfo.name;
   await fs.writeJSON(
-    path.resolve(deploymentPath, "package.json"),
+    path.resolve(deploymentRootPath, "package.json"),
     packageJSON,
     {
       spaces: "\t",
     }
   );
-  return {
-    deployment: {
-      type: "cloudflare-worker",
-    },
-  };
 }
