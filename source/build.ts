6331d763cfaa7c890f9cec220bdbd28082b0301d
Michael Kassabov <35264484+Mkassabov@users.noreply.github.com>
2023-07-04 19:11:01 -0400

    Update build.ts

--- source/build.ts
+++ source/build.ts
@@ -1,18 +1,22 @@
 import { buildWranglerToml } from "@aerofoil/af-ext-cloudflare/buildWranglerToml";
 import { getTargetEnv } from "@aerofoil/aerofoil-core/util/getTargetEnv";
-import type { ReadonlyDeep } from "type-fest";
 import { z } from "zod";
+import type { ReadonlyDeep } from "type-fest";
 
 //* import.meta.url is part of node types
 //* this deployment is for cloudflare workers so the tsconfig reflects that
 //* however this build file is run by node so we just override the type here
 interface ImportMeta {
-	url: string;
+  url: string;
 }
 
 const envValidation = z.object({});
 const env = await getTargetEnv((import.meta as ImportMeta).url, envValidation);
 
-export type EnvType = ReadonlyDeep<z.infer<typeof envValidation>>;
+const vars = {
+  ...env,
+};
 
-await buildWranglerToml([{ vars: env }]);
+export type EnvType = ReadonlyDeep<typeof vars>;
+
+await buildWranglerToml([{ vars }]);
