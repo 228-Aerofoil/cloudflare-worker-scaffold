81b0a3cbd3271af64eddf1b599297abd6ef00fb7
Michael Kassabov <35264484+Mkassabov@users.noreply.github.com>
2023-07-13 22:26:20 -0400

    dep update

--- scaffold.mjs
+++ scaffold.mjs
@@ -1,38 +1,40 @@
+import { getProjectConfig } from "@aerofoil/aerofoil-core/util/getProjectConfig";
 import { getProjectRootPath } from "@aerofoil/aerofoil-core/util/getProjectRootPath";
 import fs from "fs-extra";
 import path from "path";
 import { fileURLToPath } from "url";
 
 const sourceDirectory = path.resolve(
-  path.dirname(fileURLToPath(import.meta.url)),
-  "source"
+	path.dirname(fileURLToPath(import.meta.url)),
+	"source"
 );
 
 export async function scaffold({
-  generateDeploymentInfo,
-  generateDatabaseInfo,
-  deploymentTypes,
-  addTodos,
+	generateDeploymentInfo,
+	generateDatabaseInfo,
+	deploymentTypes,
+	addTodos,
 }) {
-  const rootPath = await getProjectRootPath();
-  const deployInfo = await generateDeploymentInfo(null, {
-    deployment: { type: "cloudflare-worker" },
-  });
-  const deploymentRootPath = path.resolve(
-    rootPath,
-    "deployments",
-    deployInfo.name
-  );
-  await fs.copy(sourceDirectory, deploymentRootPath);
-  const packageJSON = await fs.readJSON(
-    path.resolve(deploymentRootPath, "package.json")
-  );
-  packageJSON.name = deployInfo.name;
-  await fs.writeJSON(
-    path.resolve(deploymentRootPath, "package.json"),
-    packageJSON,
-    {
-      spaces: "\t",
-    }
-  );
+	const projectConfig = await getProjectConfig();
+	const rootPath = await getProjectRootPath();
+	const deployInfo = await generateDeploymentInfo(null, {
+		deployment: { type: "@cloudflare/worker" },
+	});
+	const deploymentRootPath = path.resolve(
+		rootPath,
+		"deployments",
+		deployInfo.name
+	);
+	await fs.copy(sourceDirectory, deploymentRootPath);
+	const packageJSON = await fs.readJSON(
+		path.resolve(deploymentRootPath, "package.json")
+	);
+	packageJSON.name = `@${projectConfig.name}/${deployInfo.name}`;
+	await fs.writeJSON(
+		path.resolve(deploymentRootPath, "package.json"),
+		packageJSON,
+		{
+			spaces: "\t",
+		}
+	);
 }
